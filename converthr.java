a875b0a381494b89664a5aa828769114eefad7cb
subhramanya383-star <subramanyanbhandari@gmail.com>
2025-01-31 15:21:36 +0530

    Create converthr.java

---
+++ converthr.java
@@ -0,0 +1,10 @@
+class coverthr
+  {
+    public static void main(String args[])
+    {
+      int days=10;
+      int hr;
+      hr=days*24;
+      System.out.println("covert hourn into"+hr);
+    }
+  }
